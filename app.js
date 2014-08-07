8326fda59ce2b98849e5d8930c7821e34d258839
Matt DeLuco <matt@deluco.net>
2014-08-07 15:31:02 -0400

    Removed angular-google-maps stuff.  Added double click handler to place markers on the map.

--- app.js
+++ app.js
@@ -3,22 +3,51 @@
  */
 'use strict';
 
-angular.module('maps', ['google-maps']);
+angular.module('maps', [])
 
-angular.module('maps').controller('MapsCtrl', [
+.controller('MapsCtrl', [
     '$scope',
-    function ($scope) {
-        $scope.map = {
-            center: {
-                latitude: 45,
-                longitude: -73
-            },
-            zoom: 8,
-            draggable: 'true'
-        };
+    '$timeout',
+    function ($scope, $timeout) {
 
         $scope.misc = {
-            title: "maps"
+            title: "Maps"
         };
+
+        var mapOptions = {
+            center: new google.maps.LatLng(46.50152360421799, -84.28084373474121),
+            zoom: 15,
+            draggableCursor: "crosshair",
+            disableDoubleClickZoom: true
+        };
+
+        var map = new google.maps.Map($('#map-canvas')[0], mapOptions);
+        var markers = [];
+        var clickTimeout = null;
+
+        $scope.misc.latlng = {
+            lat: map.getCenter().lat(),
+            lng: map.getCenter().lng()
+        };
+
+        google.maps.event.addListener(map, 'click', function (e) {
+            clickTimeout = $timeout(function () {
+                map.panTo(e.latLng);
+                $scope.$apply(function () {
+                    $scope.misc.latlng = {
+                        lat: e.latLng.lat(),
+                        lng: e.latLng.lng()
+                    };
+                });
+            }, 200);
+        });
+
+        google.maps.event.addListener(map, 'dblclick', function (e) {
+            $timeout.cancel(clickTimeout);
+            markers.push(new google.maps.Marker({
+                map: map,
+                position: e.latLng
+            }));
+        });
     }
 ]);
